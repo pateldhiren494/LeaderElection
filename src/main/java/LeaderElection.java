58e9e6e87eb2f90fac9b9861fa6bf4aabd14fdf9
Dhiren Patel <dhiren.patel8@ibm.com>
2021-09-28 17:34:11 -0400

    Commit project files

---
+++ src/main/java/LeaderElection.java
@@ -0,0 +1,13 @@
+import org.apache.zookeeper.*;
+
+public class LeaderElection {
+    private static String SERVER_ENDPOINT = "localhost: 2181";
+    private ZooKeeper zooKeeper;
+    public static void main(String[] args) {
+
+    }
+
+    public void connectToZookeeper() {
+
+    }
+}
